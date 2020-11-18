74141b7e5eb3285b136ce06c95e3e9bcc3cdce64
dant14 <18629421+dant14@users.noreply.github.com>
2020-11-18 13:33:21 +0000

    Create Task2.ino

---
+++ Week3/Task2/Task2.ino
@@ -0,0 +1,53 @@
+const int pingPin = 7; // Trigger Pin of Ultrasonic Sensor
+const int echoPin = 6; // Echo Pin of Ultrasonic Sensor
+const int buzzerPin = 8; // Buzzer positive pin
+
+int counter = 0;
+
+void setup() {
+   Serial.begin(115200); // Starting Serial Terminal
+   pinMode(pingPin, OUTPUT);
+   pinMode(echoPin, INPUT);
+}
+
+void loop() {
+   long duration, inches, cm;
+   
+   digitalWrite(pingPin, LOW);
+   delayMicroseconds(2);
+   digitalWrite(pingPin, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(pingPin, LOW);
+   
+   duration = pulseIn(echoPin, HIGH);
+   inches = microsecondsToInches(duration);
+   cm = microsecondsToCentimeters(duration);
+   Serial.print(inches);
+   Serial.print("in, ");
+   Serial.print(cm);
+   Serial.print("cm");
+   Serial.println();
+   delay(100);
+   if (cm > 1000)
+   {
+    //do nothing because this is the stable range of the sensor
+   }
+   else
+   {
+      beep(700, cm);
+   }
+}
+
+long microsecondsToInches(long microseconds) {
+   return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds) {
+   return microseconds / 29 / 2;
+}
+
+void beep(int note, int duration)
+{
+  //Play tone on buzzerPin
+  tone(buzzerPin, note, duration);
+}
