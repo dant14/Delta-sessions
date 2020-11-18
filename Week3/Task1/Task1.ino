0d80071a8bd377acf0f7aa3817fd53d2c88953ef
dant14 <18629421+dant14@users.noreply.github.com>
2020-11-18 15:06:51 +0000

    Update Task1.ino

--- Week3/Task1/Task1.ino
+++ Week3/Task1/Task1.ino
@@ -1,49 +1,37 @@
-const int pingPin = 7; // Trigger Pin of Ultrasonic Sensor
-const int echoPin = 6; // Echo Pin of Ultrasonic Sensor
+/*
+  Welcome to week 3 task 1.
+  In this sketch we shall see a simple example of a serial transmission 
+  where you will be able to read the output on your screen.
+
+  We will also beep THE BUZZER a couple of times 
+  so that you can see that the arduino can make some noise too.
+*/
+
 const int buzzerPin = 8; // Buzzer positive pin
+int stopFlag = 0;
 
-int counter = 0;
-
-void setup() {
-   Serial.begin(9600); // Starting Serial Terminal
-   pinMode(pingPin, OUTPUT);
-   pinMode(echoPin, INPUT);
+void setup() 
+{
+  Serial.begin(9600); // Starting Serial Terminal
 }
 
-void loop() {
-   long duration, inches, cm;
-   
-   digitalWrite(pingPin, LOW);
-   delayMicroseconds(2);
-   digitalWrite(pingPin, HIGH);
-   delayMicroseconds(10);
-   digitalWrite(pingPin, LOW);
-   
-   duration = pulseIn(echoPin, HIGH);
-   inches = microsecondsToInches(duration);
-   cm = microsecondsToCentimeters(duration);
-   Serial.print(inches);
-   Serial.print("in, ");
-   Serial.print(cm);
-   Serial.print("cm");
-   Serial.println();
-   delay(100);
-   if (cm > 1000)
-   {
-    //do nothing because it's probably an error measurement and the long beep is annoying
-   }
-   else
-   {
-      beep(700, cm);
-   }
-}
 
-long microsecondsToInches(long microseconds) {
-   return microseconds / 74 / 2;
-}
+void loop()
+{
+  if (stopFlag == 0)
+  {
+    Serial.println("Hi!!");
+    delay(500);
+    Serial.println("My name is Arduino :)");
+    delay(1000);
+    Serial.println("Beep, beep!");
 
-long microsecondsToCentimeters(long microseconds) {
-   return microseconds / 29 / 2;
+  
+    beep(700, 300); // first number is the frequency in HZ, second one is the duration of the beep in milliseconds
+    delay(500);
+    beep(700, 300);
+    stopFlag = 1;
+  }
 }
 
 void beep(int note, int duration)
